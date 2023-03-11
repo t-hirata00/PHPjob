dc3ebdbb2fe8cd5dc11f70a90cd2cfaacbc7dc73
t-hirata00 <pyude00@gmail.com>
2023-03-11 11:43:44 +0900

    3章練習問題1_再提出異

--- 3-2/index.php
+++ 3-2/index.php
@@ -1,19 +1,36 @@
 <?php 
 echo '<br>';
-$fruits = ["りんご" => "300円","みかん" => "150円","桃" => "3000円"];
-//var_dump($fruits);
-echo '<br>';
+//フルーツの名前と単価の連想配列
+$fruits = ["りんご" => 150,"みかん" => 50,"桃"=> 300];
+// var_dump($fruits);
+// echo '<br>';
 
+//フルーツの個数の配列
+$num = [2,3,10];
+// var_dump($num);
+// echo '<br>';
+
+//計算するtotal関数 引数は(単価,個数)
 function total($price,$quantity){
     $total = $price * $quantity;
     return $total;
 }
+// print "合計値は" . total(300,5) ."円";
+// echo '<br>';
 
-//print "合計値は" . total(300,5) ."円";
-
+$i=0;
 foreach ($fruits as $key => $value) {
-    # code...
-    echo $key . "は" . $value . "です。";
+    //引数の確認
+    // var_dump($key);
+    // echo '<br>';
+    // var_dump($value);
+    // echo '<br>';
+    // var_dump($num[$i]);
+    // echo '<br>';
+
+    //total実行
+    echo $key . "は" . total($num[$i],$value) . "円です。";
+    $i++;
     echo '<br>';
 }
 
