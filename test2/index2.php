1d917786c706052ce780348809f96ce41e441e35
t-hirata00 <pyude00@gmail.com>
2023-03-12 16:54:33 +0900

    Update index2.php

--- test2/index2.php
+++ test2/index2.php
@@ -1,26 +1,19 @@
 <form action="result.php" method="post">
-      お名前：<input type="text" name="my_name" /><br>
-      ご希望商品：<input type="radio" name="radio" /><br>
-      個数:
-      <select name ="kosuu" >
-        <?php for($i=1;$i <= 10; $i++ ){ ?>
-            <option value = <?php echo $i; ?>>
-            <?php echo $i ?>
-        </option>
-        <?php > }?>
-</select>
-      <?php ?>
-      <input type="radio" name="sex" value="男性">男性
-      <input type="radio" name="sex" value="女性">女性
+    お名前：<input type="text" name="my_name" />
     <br>
-      <input type="hidden" name="hidden_param" value="隠しパラメータから" />
-
-      年齢：
-    <select name="age">
-      <option value="21">21</option>
-      <option value="22">22</option>
-      <option value="23">23</option>
-      <option value="24">24</option>
-    </select>
-      <input type="submit" value="送信するよ！" />
+    ご希望商品：
+      <input type="radio" name="radio1" value="A賞" />A賞
+      <input type="radio" name="radio1" value="B賞" />B賞
+      <input type="radio" name="radio1" value="C賞" />C賞
+    <br>
+    個数:
+      <select name ="many" >
+        <?php for($i=1;$i <= 10; $i++ ){ ?>
+          <option value = <?php echo $i; ?>>
+            <?php echo $i ?>
+          </option>
+        <?php } ?>
+      </select>
+      <br>
+    <input type="submit" value="申し込み" />
     </form>
